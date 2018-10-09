8fef4fdad88337889ce66fc865352514c8c0006d
falkalus <43388421+falkalus@users.noreply.github.com>
2018-10-09 22:15:07 +0000

    verkefni1
    verkefni1

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
 /*1. Streng sem er samsettur út tveimur strengjum með + virkjanum*/
 var hello = "Ský" + " burt með þig";
 /*2. Number sem er útkoma úr reiknisdæmi sem notar eftirfarandi virkja: +, -. / og * */
-var tala = 4 + 5;
+var tala = 4 + 5 - 2 * 8 / 20;
 /*3. Boolean sem er útkoma úr samanburði sem notar eftirfarandi virkja: || (eða), && (og) og ! (ekki) */
 var bool = (true||false) && ((!false)||false);//true
 /*4. Boolean sem er útkoma úr samanburði sem notar eftirfarandi virkja: <, >, == og === */
 var bool1 = (4<6) == (5<6);// true
 /*5. Boolean sem er útkoma úr samanburði sem notar eftirfarandi virkja: <=, >= og !== */
-var bool2 = (1<=10) != (1>=10); //þetta er ekki það sama
+var bool2 = (1<=10) !== (1>=10); //þetta er ekki það sama
 /*6. */
-var hello1 = "true. aboutKalli hefur gildið kalli er góður strákur sem er sama(===) og kalli er góður strákur";
+var hello1 = "true. aboutKalli hefur gildið 'kalli er góður strákur' sem er sama(===) og 'kalli er góður strákur'";
 /*7. */
-var hello2 = "false. aboutPalli hefur gildið palli er vondur strákur sem er sama (&&) !(false)palli er vondur strákur";
-/*8. */
-var hello3 = "false";
+var hello2 = "false. aboutPalli hefur gildið palli er vondur strákur(true) && !(false)palli er vondur strákur";
+/*8. */ //!! breyta gildi í boolean
+var helloa = "a-false 0 er flase";
+var hellob = "b-true því -2 er true";
+var helloc = "c-false því "" er tómur";
+var hellod = "d-true því "" er ekki tómur";
 /*9. */
-var hello4 = "true. 5 er minna en < 9 og meira > en 2";
-/*10. */
+var hello3 = "false. þetta er ekki í sviga svo þetta er skoðað eitt í einu þegar 5<9 er true en true>2 er ekki rétt (true==1)";
 
