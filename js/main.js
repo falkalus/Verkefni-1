a3bd768cb11e1c17f6f8db0eca60d41455a1a2dc
falkalus <43388421+falkalus@users.noreply.github.com>
2018-10-09 14:20:54 +0000

    java verkefni
    java verkefni

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/*1. Streng sem er samsettur út tveimur strengjum með + virkjanum*/
+var hello = "Ský" + " burt með þig";
+/*2. Number sem er útkoma úr reiknisdæmi sem notar eftirfarandi virkja: +, -. / og * */
+var tala = 4 + 5;
+/*3. Boolean sem er útkoma úr samanburði sem notar eftirfarandi virkja: || (eða), && (og) og ! (ekki) */
+var bool = (true||false) && ((!false)||false);//true
+/*4. Boolean sem er útkoma úr samanburði sem notar eftirfarandi virkja: <, >, == og === */
+var bool1 = (4<6) == (5<6);// true
+/*5. Boolean sem er útkoma úr samanburði sem notar eftirfarandi virkja: <=, >= og !== */
+var bool2 = (1<=10) != (1>=10); //þetta er ekki það sama
+/*6. */
+var hello1 = "true. aboutKalli hefur gildið kalli er góður strákur sem er sama(===) og kalli er góður strákur";
+/*7. */
+var hello2 = "false. aboutPalli hefur gildið palli er vondur strákur sem er sama (&&) !(false)palli er vondur strákur";
+/*8. */
+var hello3 = "false";
+/*9. */
+var hello4 = "true. 5 er minna en < 9 og meira > en 2";
+/*10. */
+
